0900cd83ec295e3f39866c9724cdc442a7434a7e
Peter El Sokhn <74814481+legion0X@users.noreply.github.com>
2025-03-16 13:30:23 +0200

    modified the cors frontend access permission port

--- Backend/Assessment.API/Program.cs
+++ Backend/Assessment.API/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://localhost:3000")
+        policy => policy.WithOrigins("http://localhost:3001")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials());
